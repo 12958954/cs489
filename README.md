a335415869957107ee54a89f1161c48b45765193
Hadilo <rayking2017@outlook.com>
2023-01-30 15:37:42 -0500

    k

---
+++ README.md
@@ -0,0 +1,3 @@
+# CS 489: Build Systems Exercise
+
+See the printout for more information.
---
+++ c/output/CMakeFiles/work.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/work.dir/input.c.o"
+  "CMakeFiles/work.dir/input.c.o.d"
+  "CMakeFiles/work.dir/job.c.o"
+  "CMakeFiles/work.dir/job.c.o.d"
+  "CMakeFiles/work.dir/main.c.o"
+  "CMakeFiles/work.dir/main.c.o.d"
+  "work"
+  "work.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/work.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
